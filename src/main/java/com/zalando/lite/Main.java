57343ce818926f805f97c07026b9e79ef44dd69a
Ornella <ornelladelvicario@gmail.com>
2025-06-02 11:40:26 +0200

    Initial commit

---
+++ src/main/java/com/zalando/lite/Main.java
@@ -0,0 +1,13 @@
+package com.zalando.lite;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
